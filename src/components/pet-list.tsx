edc994bcc10a321cac3e7457564aad7e6adaaa92
Leon Wu <leon.wuuu@gmail.com>
2024-07-09 20:11:06 +1000

    added data fetching for pet list

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import React from "react";
 
-export default function PetList() {
+type PetListProps = {
+  pets: Pet[];
+};
+
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
-      <li>
-        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
-          <Image
-            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-            alt="Pet Image"
-            width={45}
-            height={45}
-            className="rounded-full object-cover"
-          />
-          <p className="font-semibold">Benjamin</p>
-        </button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet Image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
