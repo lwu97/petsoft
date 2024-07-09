685d5d4cccd7f27f0a44d66aedc2844950f98b26
Leon Wu <leon.wuuu@gmail.com>
2024-07-09 16:39:57 +1000

    app footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>app-footer</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; 2030 Leon. All rights reserved
+      </small>
+    </footer>
+  );
 }
