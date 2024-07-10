2de8bc7a7c60477e91447288ad04ae62d9174121
Leon Wu <leon.wuuu@gmail.com>
2024-07-10 12:47:40 +1000

    account page tailwind merge

--- src/components/content-block.tsx
+++ src/components/content-block.tsx
@@ -1,12 +1,22 @@
+import { cn } from "@/lib/utils";
 import React from "react";
+
+type ContentBlockProps = {
+  children: React.ReactNode;
+  className?: string;
+};
 
 export default function ContentBlock({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  className,
+}: ContentBlockProps) {
   return (
-    <div className="bg-[#F7F8Fa] shadow-sm rounded-md overflow-hidden h-full w-full">
+    <div
+      className={cn(
+        "bg-[#F7F8Fa] shadow-sm rounded-md overflow-hidden h-full w-full",
+        className
+      )}
+    >
       {children}
     </div>
   );
