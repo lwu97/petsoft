473a40278673042a887a4a51644327c6de1513c6
Leon Wu <leon.wuuu@gmail.com>
2024-10-01 18:28:06 +1000

    add loading state to sign out

--- src/components/sign-out-btn.tsx
+++ src/components/sign-out-btn.tsx
@@ -1,15 +1,22 @@
 "use client";
+
 import { logOut } from "@/actions/actions";
 import { Button } from "./ui/button";
+import { useTransition } from "react";
 
 export default function SignOutBtn() {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <Button
+      disabled={isPending}
       onClick={async () => {
-        logOut();
+        startTransition(async () => {
+          await logOut();
+        });
       }}
     >
-      Sign Out
+      Sign out
     </Button>
   );
 }
