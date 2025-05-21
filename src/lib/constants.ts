34c95c114aef977c60788421888d0f184b6096c4
Leon Wu <leon.wuuu@gmail.com>
2025-05-21 15:18:17 +1000

    update default pet image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_PET_IMAGE =
-  "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png";
+    "https://images.unsplash.com/photo-1613708257374-5e8a41485d9f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nJTIwYmxhY2t8ZW58MHx8MHx8fDI%3D";
